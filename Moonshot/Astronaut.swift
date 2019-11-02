4a599a17ab70ac7a3a74dea37899e70641ad6def
metalnodeug <metalnodeug@gmail.com>
2019-11-02 16:15:50 +0100

    First commit, import image, json file, use a file to decode JSON

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by metalnodeug on 02/11/2019.
+//  Copyright © 2019 metalnodeug. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
