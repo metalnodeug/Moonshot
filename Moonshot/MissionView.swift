a38bf2f5a4a3f9ea91c22e6609c59f8bd2181123
metalnodeug <metalnodeug@gmail.com>
2019-11-08 22:10:08 +0100

    Fixing problems with buttonStyle() and layoutPriority()

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct MissionView: View {
-
+    
     struct CrewMember {
         let role: String
         let astronaut: Astronaut
@@ -17,7 +17,7 @@ struct MissionView: View {
     
     let mission: Mission
     let astronauts: [CrewMember]
-
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical) {
@@ -27,30 +27,33 @@ struct MissionView: View {
                         .scaledToFit()
                         .frame(maxWidth: geometry.size.width * 0.7)
                         .padding(.top)
-
+                    
                     Text(self.mission.description)
                         .padding()
                     
                     ForEach(self.astronauts, id: \.role) { crewMember in
-                        HStack {
-                            Image(crewMember.astronaut.id)
-                                .resizable()
-                                .frame(width: 83, height: 60)
-                                .clipShape(Capsule())
-                                .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
-
-                            VStack(alignment: .leading) {
-                                Text(crewMember.astronaut.name)
-                                    .font(.headline)
-                                Text(crewMember.role)
-                                    .foregroundColor(.secondary)
+                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut)) {
+                            HStack {
+                                Image(crewMember.astronaut.id)
+                                    .resizable()
+                                    .frame(width: 83, height: 60)
+                                    .clipShape(Capsule())
+                                    .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
+                                
+                                VStack(alignment: .leading) {
+                                    Text(crewMember.astronaut.name)
+                                        .font(.headline)
+                                    Text(crewMember.role)
+                                        .foregroundColor(.secondary)
+                                }
+                                
+                                Spacer()
                             }
-
-                            Spacer()
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
+                        .buttonStyle(PlainButtonStyle())
                     }
-
+                    
                     Spacer(minLength: 25)
                 }
             }
@@ -60,9 +63,9 @@ struct MissionView: View {
     
     init(mission: Mission, astronauts: [Astronaut]) {
         self.mission = mission
-
+        
         var matches = [CrewMember]()
-
+        
         for member in mission.crew {
             if let match = astronauts.first(where: { $0.id == member.name }) {
                 matches.append(CrewMember(role: member.role, astronaut: match))
@@ -70,7 +73,7 @@ struct MissionView: View {
                 fatalError("Missing \(member)")
             }
         }
-
+        
         self.astronauts = matches
     }
 }
@@ -78,7 +81,7 @@ struct MissionView: View {
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
-
+    
     static var previews: some View {
         MissionView(mission: missions[0], astronauts: astronauts)
     }
